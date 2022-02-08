c584c60b034d115da45f366101aa7a1733d0f43d
eastnile <eastnileuc@gmail.com>
2022-02-08 13:05:33 -0500

    test1

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,17 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' Load a Matrix
+#'
+#' This function loads a file as a matrix. It assumes that the first column
+#' contains the rownames and the subsequent columns are the sample identifiers.
+#' Any rows with duplicated row names will be dropped with the first one being
+#' kepted.
+#'
+#' @param infile Path to the input file
+#' @return A matrix of the infile
+#' @export
+
 hello <- function() {
   print("Hello, world!")
 }
