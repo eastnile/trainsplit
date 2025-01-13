8012305b904a9cec0c397cf71d9616b1c4dff32d
eastnile <eastnileuc@gmail.com>
2025-01-12 19:25:05 -0500

    update

--- R/trainsplit.R
+++ R/trainsplit.R
@@ -1,16 +1,17 @@
 #' @title trainsplit
-#' @description Splits a dataframe, tibble, or data.table into a test set and training set. Specify either the number or percentage of observations to put into training set.
+#' @description Splits a dataframe, tibble, or data.table into a test set and training set. Specify either the number or percentage of observations to be put into training set.
 #'
 #' @param data The dataset you want to split
 #' @param ntrain The number of observations to go into the training set. Must be >= 0 and <= nrow(data).
 #' @param trainpct Fraction of observations to go into training set. Must be >= 0 and =< 1. If set to 0 or 1, the empty test or training set will still inherit the same column names and types as the original dataset.
 #' @param round_ntrain What to do when nrow(data) * trainpct is not a whole number. Default behavior is to round the size of the training set. Use 'ceiling' or 'floor' to instead set the size of training set to next highest or lowest whole number.
 #' @param seed Sets the random seed; use this argument if you want to always get the same result. Note: sets seed only locally within the function.
+#' @param return Three return modes available: "parentenv" assigns the training and test sets into the environment that called the function with names based on the name of the original dataset; this is intended largely for an educational context. "list" will return a list with the training and test sets. "index" will return only the numerical index of the rows to be placed into the training set, which can then be manually subset by the user.
 #'
-#' @return A list, containing the training and test sets.
+#' @return Depends on "return" argument; either a list, an index, or NULL if return = "parentenv" was selected.
 #'
 #' @examples
-#' # Returns a list containing the training and test sets:
+#' # Splits the training and test sets and assigns them into memory.
 #' trainsplit(mtcars, trainpct = 0.75)
 #' # Specify size of training set by number of rows, not percent:
 #' trainsplit(mtcars, ntrain = 10)
@@ -18,76 +19,83 @@
 #' trainsplit(mtcars, trainpct = 0.01, round_ntrain = 'ceiling')
 #' # Also works with data.table:
 #' trainsplit(data.table::as.data.table(mtcars), trainpct = 0.75)
+#' # Return a list containing the training/test sets instead:
+#' trainsplit(mtcars, trainpct = 0.75, return = 'list')
 
 #' @export
 #' @importFrom data.table copy as.data.table
 
 trainsplit = function(data, ntrain = NULL, trainpct = NULL,
-                      round_ntrain = 'round', seed = NULL) {
-
-   if (is.null(ntrain) & is.null(trainpct)) {
-      stop('Must specify desired size or percentage of data to go into training set.')
-   }
-
-   if (!is.null(ntrain) & !is.null(trainpct)) {
-      stop('Must specify either ntrain or trainpct, but not both')
-   }
-
-   if (!is.null(ntrain)) {
-      if (ntrain <= 0 | ntrain >= nrow(data)) {
-         stop('ntrain must be between 0 and nrow(data)')
-      }
-   }
-   if (!is.null(trainpct)) {
-      if (trainpct <= 0 | trainpct >= 1) {
-         stop('trainpct must be between 0 and 1')
-      }
-      if (!round_ntrain %in% c('round', 'floor', 'ceiling')) {
-         stop('round_ntrain must be "round", "floor", or "ceiling"')
-      }
-      if (round_ntrain == 'ceiling') {
-         ntrain = ceiling(trainpct*nrow(data))
-      } else if (round_ntrain == 'floor') {
-         ntrain = floor(trainpct*nrow(data))
-      } else {
-         ntrain = round(trainpct*nrow(data), digits = 0)
-      }
-   }
+                      round_ntrain = 'round', seed = NULL,
+                      return = 'parentenv') {
 
    if (!is.null(seed)) {
       set.seed(seed)
    }
+   if (is.null(ntrain) & is.null(trainpct)) {
+      stop('Must specify either ntrain or trainpct.')
+   }
+   if (!is.null(ntrain) & !is.null(trainpct)) {
+      stop('Must specify either ntrain or trainpct, but not both')
+   }
 
-   output <- list()
-   index_train <- sample.int(n = nrow(data), size = ntrain, replace = F)
-
-   if ('data.table' %in% class(data)) {
-      if (ntrain == 0) {
-         output$train = data.table::copy(data[integer(0), ]) # keep structure of data even if train or test set is empty
-         output$test = data.table::copy(data) # Copy() needed to prevent reference error
-      } else if (ntrain == nrow(data)) {
-         output$train = data.table::copy(data)
-         output$test = data.table::copy(data[integer(0), ])
-      } else {
-         output$train <- data.table::copy(data[index_train, ])
-         output$test <- data.table::copy(data[-index_train, ])
+   # Determine ntrain based on trainpct
+   if (!is.null(trainpct)) {
+      if (trainpct <= 0 | trainpct >= 1) {
+         stop('trainpct must be between 0 and 1')
       }
-   } else {
-      if (ntrain == 0) {
-         output$train = data[integer(0), ]
-         output$test = data
-      } else if (ntrain == nrow(data)) {
-         output$train = data
-         output$test = data[integer(0), ]
+      if (identical(round_ntrain,'ceiling')) {
+         ntrain = ceiling(trainpct*nrow(data))
+      } else if (identical(round_ntrain,'floor')) {
+         ntrain = floor(trainpct*nrow(data))
+      } else if (identical(round_ntrain, 'round')) {
+         ntrain = round(trainpct*nrow(data), digits = 0)
       } else {
-         output$train <- data[index_train, ]
-         output$test <- data[-index_train, ]
+        stop('round_ntrain must be "round", "floor", or "ceiling"')
       }
    }
 
-   rownames(output$train) = NULL
-   rownames(output$test) = NULL
-   stopifnot(nrow(output$train)+nrow(output$test) == nrow(data))
-   return(output)
+   if (ntrain < 0 | ntrain > nrow(data)) {
+      stop('ntrain must be between 0 and nrow(data)')
+   }
+
+   # Split data
+   index_train <- sample.int(n = nrow(data), size = ntrain, replace = F)
+   if (ntrain == 0) {
+      train = data[integer(0), ]
+      test = data
+   } else if (ntrain == nrow(data)) {
+      train = data
+      test = data[integer(0), ]
+   } else {
+      train <- data[index_train, ]
+      test <- data[-index_train, ]
+   }
+   rownames(train) = NULL
+   rownames(test) = NULL
+   stopifnot(nrow(train) + nrow(test) == nrow(data))
+
+   # Return modes
+   if (identical(return, 'parentenv')) {
+      origname = deparse(substitute(data))
+      if ('data.table' %in% class(data)) { # Prevents shallow copy warning when using predict() after split
+         assign(paste0(origname, '_train'), copy(train), pos = parent.frame())
+         assign(paste0(origname, '_test'), copy(test), pos = parent.frame())
+      } else {
+         assign(paste0(origname, '_train'), train, pos = parent.frame())
+         assign(paste0(origname, '_test'), test, pos = parent.frame())
+      }
+      return(invisible(NULL))
+   }
+
+   if (identical(return,'list')) {
+      return(list(train = train, test = test))
+   }
+
+   if (identical(return,'index')) {
+      return(index_train)
+   }
+
+   stop('"return" must be "parentenv", "list", or "index"')
 }
 
--- tests/testthat/test-trainsplit.R
+++ tests/testthat/test-trainsplit.R
@@ -4,19 +4,19 @@ tb = tibble::as_tibble(df)
 
 test_that("package splits dataset correctly", {
 
-   split = trainsplit(df, ntrain = 10, seed = 123)
+   split = trainsplit(df, ntrain = 10, seed = 123, return = 'list')
    expect_equal(nrow(split$train), 10)
    expect_equal(names(df), names(split$train))
 
-   split = trainsplit(df, trainpct = .75, seed = 123)
+   split = trainsplit(df, trainpct = .75, seed = 123, return = 'list')
    expect_equal(nrow(split$train), 20)
    expect_equal(names(df), names(split$train))
 
-   split = trainsplit(df, trainpct = .01, seed = 123)
+   split = trainsplit(df, trainpct = .01, seed = 123, return = 'list')
    expect_equal(nrow(split$train), 0)
    expect_equal(names(df), names(split$train))
 
-   split = trainsplit(df, trainpct = .01, seed = 123, round_ntrain = 'ceiling')
+   split = trainsplit(df, trainpct = .01, seed = 123, round_ntrain = 'ceiling', return = 'list')
    expect_equal(nrow(split$train), 1)
    expect_equal(names(df), names(split$train))
 
@@ -24,15 +24,20 @@ test_that("package splits dataset correctly", {
 
 test_that("package works with dt and tb", {
 
-   split = trainsplit(dt, trainpct = .75, seed = 123)
+   split = trainsplit(dt, trainpct = .75, seed = 123, return = 'list')
    expect_equal(nrow(split$train), 20)
    expect_equal(names(df), names(split$train))
 
-   split = trainsplit(tb, trainpct = .75, seed = 123)
+   split = trainsplit(tb, trainpct = .75, seed = 123, return = 'list')
    expect_equal(nrow(split$train), 20)
    expect_equal(names(df), names(split$train))
 
 })
 
 
+# Check for ref error that sometimes occurs with data.table
+iris = data.table::as.data.table(iris)
+trainsplit(iris, trainpct = .75, seed = 123)
+mod = lm(Sepal.Length ~ ., data = iris_train)
+expect_no_condition(iris_train[, yhat := predict(mod, newdata = iris_train)])
 
